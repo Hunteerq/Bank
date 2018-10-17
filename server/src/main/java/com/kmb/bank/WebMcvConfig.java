c68a9bc87ec16f16cc047ba42b49cfb957b9eb31
sasuuu <37144833+sasuuu@users.noreply.github.com>
2018-10-17 12:12:52 +0200

    New login with Spring Security and Thymeleaf

--- server/src/main/java/com/kmb/bank/WebMcvConfig.java
+++ server/src/main/java/com/kmb/bank/WebMcvConfig.java
@@ -2,25 +2,18 @@ package com.kmb.bank;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+
 
 @Configuration
 @EnableWebMvc
 public class WebMcvConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
     }
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
