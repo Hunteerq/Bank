264c3b2dc6c7f6d6a4bfd7e74d9520cc012bed89
Artur Kulig <hunteerq@icloud.com>
2018-12-01 17:22:10 +0100

    Adding working balance updateds with transaction

--- transaction_logger/src/main/java/com/kmb/transactionlogger/Listener.java
+++ transaction_logger/src/main/java/com/kmb/transactionlogger/Listener.java
@@ -1,12 +1,14 @@
 package com.kmb.transactionlogger;
 
-
 import com.kmb.transactionlogger.models.TransferDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 
 @Log4j2
@@ -16,15 +18,53 @@ public class Listener  {
     @Autowired
     private Jackson2JsonMessageConverter jackson2JsonMessageConverter;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RabbitListener(queues = "${rabbitmq.queue}")
     public void listen(TransferDTO transfer) {
-        try {
-            log.info(transfer);
-        } catch (Exception e) {
-            log.debug("Error thrown while listening + " + e.getMessage());
+
+        if(updateDatabase(transfer)) {
+            log.info("Modifying tables completed");
+        } else  {
+            log.error("Error modifying tables");
         }
 
     }
 
+    private boolean updateDatabase(TransferDTO transferDTO) {
+        try {
+            Double currentAmount = jdbcTemplate.queryForObject("SELECT account.balance FROM account " +
+                    "WHERE account.number = ?", new Object[]{transferDTO.getUserAccountNumber() }, Double.class);
 
+            if ((currentAmount != null && currentAmount - transferDTO.getAmount() >= 0)) {
+                return updateTables(transferDTO);
+            } else {
+                log.info("Not enough funds");
+                return false;
+            }
+        } catch(Exception e) {
+            log.error("Error asking for balance " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Transactional
+    public boolean updateTables(TransferDTO transferDTO)  {
+        try {
+            jdbcTemplate.update("UPDATE account " +
+                            "SET balance = balance - ? WHERE number = ?",
+                    new Object[]{transferDTO.getAmount(), transferDTO.getUserAccountNumber()});
+
+            jdbcTemplate.update("UPDATE account " +
+                            "SET balance = balance + ? WHERE number = ?",
+                    new Object[]{transferDTO.getAmount(), transferDTO.getRecipientAccountNumber()});
+
+            log.info("Databases successfully updated");
+        } catch(Exception e) {
+            log.error("Erorr updating tables " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
 }
--- transaction_logger/src/main/java/com/kmb/transactionlogger/config/ListenerBeans.java
+++ transaction_logger/src/main/java/com/kmb/transactionlogger/config/ListenerBeans.java
@@ -1,8 +1,6 @@
 package com.kmb.transactionlogger.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kmb.transactionlogger.models.TransferDTO;
-import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,11 +16,6 @@ public class ListenerBeans {
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        Jackson2JsonMessageConverter  jackson2JsonMessageConverter= new Jackson2JsonMessageConverter(objectMapper());
-        DefaultClassMapper classMapper = new DefaultClassMapper();
-        classMapper.setDefaultType(TransferDTO.class);
-        classMapper.setDefaultMapClass(TransferDTO.class);
-        jackson2JsonMessageConverter.setClassMapper(classMapper);
         return new Jackson2JsonMessageConverter(objectMapper());
     }
 
